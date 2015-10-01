35ded6e5597447346ecd281e15269d347b2c7fb5
David Bain <david@alteroo.com>
2015-09-30 23:48:35 -0500

    set Plone vhm

--- tryplone.py
+++ tryplone.py
@@ -1,1 +1,7 @@
+from os import getenv
 print "Plone is installed"
+URL=getenv('URL')
+HOSTNAME = URL.split(".")[0]
+import transaction
+app.virtual_hosting.set_map('%s/Plone' % HOSTNAME)
+transaction.commit()
